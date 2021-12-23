5ff4a79c21182ace13f31b5872f6ba4d2ea4d7f8
Neymartri <Khoaleo2002@gmail.com>
2021-12-23 16:46:10 +1000

    Connect postgreSQL db to back end via node-postgres, implement environment variables to prevent sensitive data hard-coded, tested the db sucessfully connected on PostMan

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 // set environment variable and export express
 require("dotenv").config(); 
 const express = require("express");
+const morgan = require("morgan");
+//Import database from PostgreSQL
+const db = require('./db')
 
 const app = express();
 
-// middleware for route handling
-app.use((req, res, next) => {
-    console.log("middleware running")
-    next();
-});
+// middleware for route handling via morgan 
+ app.use(express.json());
 
 // Get all events route
-app.get("/api/v1/events", (req, res) => {
-    console.log("route handler running");
+app.get("/api/v1/events", async (req, res) => {
+
+    const results = await db.query("select * from events")
+    console.log(results);
     res.status(200).json({
         status: "success",
         data: {
-            events: ["pyjamas party", "food festival", "night market"],
+            event: ["pyjamas party", "food festival", "night market"],
         }, 
     });
 });
@@ -24,13 +26,46 @@ app.get("/api/v1/events", (req, res) => {
 //Get an Event
 app.get("/api/v1/events/:id", (req, res) => {
     console.log(req.params);
+
+    res.status(200).json({
+        status: "success", 
+        data: {
+            event: "night market"
+        },
+    });
 }); 
 
 //Create an Event
 app.post("/api/v1/events", (req, res) => {
-    console.log(req);
+    console.log(req.body);
+
+    res.status(201).json({
+        status: "success", 
+        data: {
+            event: "night market",
+        },
+    });
 });
 
+// Update an Event
+app.put("/api/v1/events/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body)
+
+    res.status(200).json({
+        status: "success", 
+        data: {
+            event: "night market",
+        },
+    });
+});
+
+// Delete an event 
+app.delete("/api/v1/events/:id", (req, res) => {
+    res.status(204).json({
+        status: "success",
+    });
+})
 
 //create sever listener
 const port = process.env.PORT || 3005; 

604a4180508ce69d2ebb3cb994ad5e15845898d7
Neymartri <Khoaleo2002@gmail.com>
2022-01-06 14:31:42 +1000

    Create table for EventList and add to Home Component

---
+++ src/components/EventList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const EventList = () => {
+    return (
+        <div className="list-group">
+            <table className="table table-hover table-dark">
+                <thead>
+                    <tr className="bg-primary">
+                        <th scope="col">Events</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Price Range</th>
+                        <th scope="col">Ratings</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>SouthBank Walk</td>
+                        <td>SouthBank </td>
+                        <td> $$ </td>
+                        <td>Rating</td>
+                        <td><button className="btn btn-warning">Update</button></td>
+                        <td><button className="btn btn-warning">Delete</button></td>
+                    </tr>
+                </tbody>
+             </table>
+            
+        </div>
+    )
+}
+
+export default EventList
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AddEvent from '../components/AddEvent';
+import EventList from '../components/EventList';
 import Header from '../components/Header';
 
 //Connect Header to Home Component
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Header/>
             <AddEvent/>
+            <EventList/>
         </div>
     );
 };
